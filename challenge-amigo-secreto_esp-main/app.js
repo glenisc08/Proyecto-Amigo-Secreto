2688407fe6d8def7b9a29be5b3d617f98846d49f
Glenis <glenisc163@gmail.com>
2025-02-04 23:09:36 -0500

    Agregando archivos del proyecto

---
+++ challenge-amigo-secreto_esp-main/app.js
@@ -0,0 +1,53 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() {
+    // Capturar el valor del campo de entrada
+    const inputAmigo = document.getElementById('amigo');
+    const nombre = inputAmigo.value;
+
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+
+    amigos.push(nombre);
+
+    inputAmigo.value = '';
+
+    actualizarListaAmigos();
+}
+
+function actualizarListaAmigos() {
+    // Obtener el elemento de la lista
+    const lista = document.getElementById('listaAmigos');
+    
+    lista.innerHTML = '';
+    
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement('li');
+        
+        li.textContent = amigos[i];
+        
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('No hay amigos en la lista para sortear');
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    const resultado = document.getElementById('resultado');
+
+    resultado.innerHTML = '';
+    const li = document.createElement('li');
+    li.textContent = `Amigo sorteado: ${amigoSorteado}`;
+    resultado.appendChild(li);
+}
